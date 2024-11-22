0e65862dcfe9b730f33402734e1e60567c8c78bd
joaoPedroBM <joaopedrobarcelllosmoura@gmail.com>
2024-11-22 16:09:59 -0300

    refactor: distributing responsibilities mvc format

--- server.js
+++ server.js
@@ -1,45 +1,12 @@
 import express from "express";
-import conectarAoBanco from "./src/config/dbConfig.js";
 import dotenv from 'dotenv';
+import routes from "./src/routes/postsRoutes.js";
 
 dotenv.config();
 
-const conexao = await conectarAoBanco(process.env.STRING_CONEXAO); 
-
 const app = express();
-app.use(express.json());
+routes(app);
 
 app.listen(3000,()=> {
 console.log("Servidor escutando...");
 });
-
-/*  Old search method
-function buscarPostPorID(id){
-  return posts.findIndex((post)=>{
-     return post.id === Number(id);
-  });
-};
-*/
-
-async function getTodosPosts(){
-  const db = conexao.db("conteudo_rede_social");
-  const colecao = db.collection("posts");
-
-  return colecao.find().toArray();
-};
-
-app.get("/posts",async (req,res)=>{ //Oque vem depois do '/:' será o atributo referência
-    const posts = await getTodosPosts();
-    res.status(200).json(posts);
-});
-
-app.get("/livros",(req,res)=>{
-    
-    const livro1={
-        titulo: "Evangelho falsificado",
-        autor: "Roger E. Olson",
-        opiniao:"Excelente Livro"
-    }
-    
-    res.status(200).send(livro1);
-})
