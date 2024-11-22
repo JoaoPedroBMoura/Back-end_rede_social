65970b89b055148296db411677cb4ed001b885ba
joaoPedroBM <joaopedrobarcelllosmoura@gmail.com>
2024-11-22 15:10:25 -0300

    feat: add direct communication method with db

--- server.js
+++ server.js
@@ -4,49 +4,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-await conectarAoBanco(process.env.STRING_CONEXAO); 
-
-const posts = [
-    {
-        id:1,
-        descricao: "Uma foto teste",
-        imagem: "https://placecats.com/millie/300/150",
-        local:"internet",
-        data:"20/11/2024"
-    },
-    {
-        id:2,
-        descricao: "Paisagem deslumbrante ao pôr do sol",
-        imagem: "https://picsum.photos/seed/1/300/200",
-        local: "Praia de Copacabana 2",
-        data: "2023-11-20"
-    },
-    {
-        id:3,
-        descricao: "Meu gato fazendo cara de bobo",
-        imagem: "https://placekitten.com/200/300",
-        categoria: "Animais"
-    },
-    {
-        id:4,
-        descricao: "Receita deliciosa de bolo de chocolate",
-        imagem: "https://loremflickr.com/320/240/food",
-        categoria: "Comida",
-        ingredientes: ["chocolate", "ovos", "farinha", "leite"]
-    },
-    {
-        id:5,
-        descricao: "Citação inspiradora sobre a vida",
-        autor: "Albert Einstein",
-        texto: "A imaginação é mais importante que o conhecimento."
-    },
-    {
-        id:6,
-        descricao: "Vídeo de uma música épica",
-        video: "https://www.youtube.com/watch?v=dQw4w9WgXcQ", 
-        duracao: "3:32"
-    }
-];
+const conexao = await conectarAoBanco(process.env.STRING_CONEXAO); 
 
 const app = express();
 app.use(express.json());
@@ -55,15 +13,24 @@ app.listen(3000,()=> {
 console.log("Servidor escutando...");
 });
 
+/*  Old search method
 function buscarPostPorID(id){
   return posts.findIndex((post)=>{
      return post.id === Number(id);
   });
-}
+};
+*/
 
-app.get("/posts/:id",(req,res)=>{ //Oque vem depois do '/:' será o atributo referência
-    const index = buscarPostPorID(req.params.id) //Usei 'id' porque é o atributo desejado
-    res.status(200).json(posts[index]);
+async function getTodosPosts(){
+  const db = conexao.db("conteudo_rede_social");
+  const colecao = db.collection("posts");
+
+  return colecao.find().toArray();
+};
+
+app.get("/posts",async (req,res)=>{ //Oque vem depois do '/:' será o atributo referência
+    const posts = await getTodosPosts();
+    res.status(200).json(posts);
 });
 
 app.get("/livros",(req,res)=>{
