4f792d3b304d502133e616127c8b6201230ee5c3
joaoPedroBM <joaopedrobarcelllosmoura@gmail.com>
2024-11-23 22:10:46 -0300

    feat: add multer

--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,6 +1,16 @@
 import express from "express";
+import multer from "multer";
 import { listaPosts, postarNovoPost} from "../controller/postsController.js";
 
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+    cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+    cb(null, file.originalname);
+    }
+  })
+  const upload = multer({ dest: "./uploads" , storage}) // aonde ficarão salvas nosssas imagens temp
 //local aonde ficaram armazenadas todas as rotas da aplicação
 const routes = (app)=>{
     //permite que o serviro interprete requisições com corpo JSON
@@ -10,6 +20,8 @@ const routes = (app)=>{
     app.get("/posts",listaPosts);
     //rota para criar um novo post
     app.post("/posts",postarNovoPost);
+    //rota para mandar imagens
+    app.post("/upload",upload.single("imagem"),uploadImagem);
 
     app.get("/livros",(req,res)=>{
     
