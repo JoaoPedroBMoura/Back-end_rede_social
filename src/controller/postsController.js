ca8c0d661d8570c3b58cf580f618ac905bd949ca
joaoPedroBM <joaopedrobarcelllosmoura@gmail.com>
2024-11-23 21:23:21 -0300

    feat: add new method for put new post

--- src/controller/postsController.js
+++ src/controller/postsController.js
@@ -6,3 +6,14 @@ export async function listaPosts(req,res){
     res.status(200).json(posts);
 };
     
+export async function postarNovoPost(req,res) {
+    const novoPost = req.body;
+
+    try{
+        const postCriado = await criarPost(novoPost);
+        res.status(200).json(postCriado)
+    } catch{
+        console.error(error.message);
+        res.status(500).json({"Erro":"Falha na requisição"});
+    }
+}
